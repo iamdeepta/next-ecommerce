f070b14b3897c1502f73d592fc474bccdd31971f
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2021-08-27 22:48:00 +0600

    updated

---
+++ pages/api/AppUrl.js
@@ -0,0 +1,15 @@
+export class AppUrl {
+
+    //static base_url = "http://localhost/BackendTrustlite/";
+    static base_url = "https://www.trust-lite.com/BackendTrustlite1/";
+}
+
+//export default AppUrl;
+import React from 'react'
+
+const Demo = () => {
+
+    return(<></>);
+}
+
+export default Demo;
